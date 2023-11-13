b1b84fcab0241a04f75a42a757851a1c66c2b4f4
tifufu <dematasjoshua@hotmail.com>
2023-11-13 23:55:39 +0100

    Updated test component to loading image into an array of shade widgets.

--- src/test-components/App.jsx
+++ src/test-components/App.jsx
@@ -4,39 +4,19 @@ import Dialog from "../components/Dialog";
 import Break from "../components/Break";
 import Label from "../components/Label";
 import Separator from "../components/Separator";
-
-const defColors = [
-  { red: 255, green: 0, blue: 0 },
-  { red: 0, green: 255, blue: 0 },
-  { red: 0, green: 0, blue: 255 },
-  { red: 255, green: 255, blue: 0 },
-  { red: 255, green: 0, blue: 255 },
-  { red: 0, green: 255, blue: 255 },
-  { red: 255, green: 255, blue: 255 },
-  { hue: 120, saturation: 0, brightness: 0 },
-];
-
-function swapRandomColors(arg) {
-  const colors = [...arg];
-  const a = Math.floor(Math.random() * colors.length);
-  const b = Math.floor(Math.random() * colors.length);
-  const tmp = colors[a];
-  colors[a] = colors[b];
-  colors[b] = tmp;
-  return colors;
-}
+import getColors from "./get-colors";
 
 export default function () {
   const [count, setCount] = React.useState(0);
-  const [colors, setColors] = React.useState(defColors);
+  const [colors, setColors] = React.useState(null);
 
   React.useEffect(() => {
-    const interval = setInterval(() => {
-      setColors((old) => swapRandomColors(old));
-      console.log("Colors changed");
-    }, 1000);
-    return () => clearInterval(interval);
-  });
+    const loadShades = async () => {
+      const colors = await getColors();
+      setColors(colors);
+    };
+    loadShades();
+  }, []);
 
   function addClicked() {
     setCount((count) => {
@@ -63,16 +43,10 @@ export default function () {
       <Button text="+" onclick={addClicked} />
       <Separator text="The button below does nothing" />
       <Button text="Does nothing" focus={true} />
-      <Break />
-      <shades colors={colors} mode="sort" onclick={shadesClicked} />
-      <Break />
-      <shades colors={colors} mode="sort" />
-      <Break />
-      <shades colors={colors} mode="sort" />
-      <Break />
-      <shades colors={colors} mode="sort" />
-      <Break />
-      <shades colors={colors} mode="sort" />
+      <Break />      
+      {colors && colors.map((colorsRow) => (
+        <><shades colors={colorsRow} mode="sort" onclick={shadesClicked} /><newrow /></>
+      ))}
     </Dialog>
   );
 }
